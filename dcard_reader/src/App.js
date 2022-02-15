e6f600ed29f360f5fe2fee19df1e7395df06cc5f
Luciano <majorass206@gmail.com>
2022-02-15 10:23:18 +0800

    adjust layout

--- dcard_reader/src/App.js
+++ dcard_reader/src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="container mx-auto bg-blue-900">
-      <h1 className="text-3xl font-bold text-red-600">Hello World</h1>
+    <div className="box-border bg-blue-900 py-2">
+      <div className="container mx-auto ">
+        <div className="p-4 rounded-lg shadow-md text-center bg-white">
+          <h3>title</h3>
+          <p>contect</p>
+        </div>
+      </div>
     </div>
   );
 }
